67a2dae3a152629e2f4daca39af3894bb82f65d9
Vladimir <sirbuvova4@gmail.com>
2022-08-13 13:51:24 +0300

    add first method in Controller

--- src/main/java/com/example/gittest/controller/MainController.java
+++ src/main/java/com/example/gittest/controller/MainController.java
@@ -1,7 +1,13 @@
 package com.example.gittest.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello World!";
+    }
 }
