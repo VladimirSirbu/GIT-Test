0140d6bc4af5fabc4bb0c622113caa5e37a74c44
Vladimir <sirbuvova4@gmail.com>
2022-08-13 18:58:06 +0300

    move ProductService.java into services package + rebasing

--- src/main/java/com/example/gittest/services/ProductService.java
+++ src/main/java/com/example/gittest/services/ProductService.java
@@ -1,4 +1,12 @@
 package com.example.gittest.services;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProductService {
+    public List<String> getAllProducts() {
+        return List.of("A", "B", "C", "D");
+    }
 }
